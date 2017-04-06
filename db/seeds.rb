6ea61df46d10ab084c8baa790d649549174fd1d1
Ryutaro <ricken0203@gmail.com>
2017-04-06 17:16:00 +0900

    [update] seeds.rbにLocationのaddressをセット

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create(:address => '東京都港区芝公園4丁目2-8') #東京タワー
+Location.create(:address => '東京都墨田区押上1丁目1−2') #スカイツリー
+Location.create(:address => '千葉県千葉市美浜区中瀬2−1') #幕張メッセ
