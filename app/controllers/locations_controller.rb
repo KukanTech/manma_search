6f3013babcbdf3b8b3c59c8704c0117a287e03d3
Ryutaro <ricken0203@gmail.com>
2017-04-07 17:56:07 +0900

    [update] search.htmlに学生と家族の住所情報を出力

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,22 +9,24 @@ class LocationsController < ApplicationController
   def search
     if params[:address].present?
       address = params[:address]
+      @inputaddress = address
 
       ## 1. 出発地点と目的地点の設定
       currentPlaceLat = Geocoder.search(address)[0].geometry["location"]["lat"]
       currentPlaceLon = Geocoder.search(address)[0].geometry["location"]["lng"]
 
-      @locations = Location.all
-      destinationLat = @locations.find(1).latitude
-      destinationLon = @locations.find(1).longitude
+      locations = Location.all
+      @familyaddress = locations.find(1).address
+      destinationLat = locations.find(1).latitude
+      destinationLon = locations.find(1).longitude
 
       ## 2. OSRM APIへアクセスし、結果を取得
       endpointUrl = "http://router.project-osrm.org/route/v1/driving/#{currentPlaceLon},#{currentPlaceLat};#{destinationLon},#{destinationLat}"
-      response =  HTTParty.get(URI.encode(endpointUrl))
+      @response =  HTTParty.get(URI.encode(endpointUrl))
 
       ## 3. 取得した結果を出力
-      puts "所要時間：#{response["routes"][0]["duration"]}秒"
-      puts "距離：#{response["routes"][0]["distance"]}m"
+      puts "所要時間：#{@response["routes"][0]["duration"]}秒"
+      puts "距離：#{@response["routes"][0]["distance"]}m"
 
     end
   end
