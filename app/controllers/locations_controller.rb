58a79776507ef10792c396a1ce8fdbd431e789ef
Ryutaro <ricken0203@gmail.com>
2017-04-07 15:07:34 +0900

    [update] 住所を入力したときに緯度、経度が表示される機能を実装

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,12 @@
 class LocationsController < ApplicationController
 
-  def nearby
-    @locations = Location.near([params[:location][:latitude], params[:location][:longtitude]], 50)
+  def search
+    if params[:address].present?
+      address = params[:address]
+      puts address
+      @latitude = Geocoder.search(address)[0].geometry["location"]["lat"]
+      @longitude = Geocoder.search(address)[0].geometry["location"]["lng"]
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/locations/search', to: 'locations#search'
+  get 'locations/search'
 
 end
