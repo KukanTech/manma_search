d00f48b30ff652e39592278648c3d8824eddd1cd
Ryutaro <ricken0203@gmail.com>
2017-04-07 18:29:46 +0900

    [update] データベースにある全ての住所に対して距離・時間を算出(ソートは未実装)　

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -12,22 +12,10 @@ class LocationsController < ApplicationController
       @inputaddress = address
 
       ## 1. 出発地点と目的地点の設定
-      currentPlaceLat = Geocoder.search(address)[0].geometry["location"]["lat"]
-      currentPlaceLon = Geocoder.search(address)[0].geometry["location"]["lng"]
+      @currentPlaceLat = Geocoder.search(address)[0].geometry["location"]["lat"]
+      @currentPlaceLon = Geocoder.search(address)[0].geometry["location"]["lng"]
 
-      locations = Location.all
-      @familyaddress = locations.find(1).address
-      destinationLat = locations.find(1).latitude
-      destinationLon = locations.find(1).longitude
-
-      ## 2. OSRM APIへアクセスし、結果を取得
-      endpointUrl = "http://router.project-osrm.org/route/v1/driving/#{currentPlaceLon},#{currentPlaceLat};#{destinationLon},#{destinationLat}"
-      @response =  HTTParty.get(URI.encode(endpointUrl))
-
-      ## 3. 取得した結果を出力
-      puts "所要時間：#{@response["routes"][0]["duration"]}秒"
-      puts "距離：#{@response["routes"][0]["distance"]}m"
-
+      @locations = Location.all
     end
   end
 
