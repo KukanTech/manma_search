c126b536bca90863467b968f79e2f7ab72416f6d
Ryutaro <ricken0203@gmail.com>
2017-04-10 17:28:35 +0900

    [update] APIを使わずに、二点間の距離計算とソート表示を実装

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,15 +8,34 @@ class LocationsController < ApplicationController
 
   def search
     if params[:address].present?
-      address = params[:address]
-      @inputaddress = address
+      currentPlace = params[:address]
+      @inputaddress = currentPlace
 
       ## 1. 出発地点と目的地点の設定
-      @currentPlaceLat = Geocoder.search(address)[0].geometry["location"]["lat"]
-      @currentPlaceLon = Geocoder.search(address)[0].geometry["location"]["lng"]
+      currentPlaceLat = Geocoder.search(currentPlace)[0].geometry["location"]["lat"]
+      currentPlaceLon = Geocoder.search(currentPlace)[0].geometry["location"]["lng"]
 
-      @locations = Location.all
+      candidateList = {}
+      locations = Location.all
+
+      locations.each do |n|
+        familyaddress = n.address
+        destinationLat = n.latitude
+        destinationLon = n.longitude
+
+        ## 2. 二点間の距離計算
+        y1 = currentPlaceLat * Math::PI / 180
+        x1 = currentPlaceLon * Math::PI / 180
+        y2 = destinationLat * Math::PI / 180
+        x2 = destinationLon * Math::PI / 180
+        earth_r = 6378140
+
+        deg = Math::sin(y1) * Math::sin(y2) + Math::cos(y1) * Math::cos(y2) * Math::cos(x2 - x1)
+        distance = earth_r * (Math::atan(-deg / Math::sqrt(-deg * deg + 1)) + Math::PI / 2) / 1000
+
+        candidateList.store(familyaddress, distance)
+      end
+      @candidateList= candidateList.sort {|(k1, v1), (k2, v2)| v1 <=> v2 }
     end
   end
-
 end
