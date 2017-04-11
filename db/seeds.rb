a41fd73605803009097bc5b39b5b6e6797fcdef2
Ryutaro <ricken0203@gmail.com>
2017-04-12 01:25:35 +0900

    [update] seeds.rbにデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 Location.create(:address => '東京都港区芝公園4丁目2-8') #東京タワー
 Location.create(:address => '東京都墨田区押上1丁目1−2') #スカイツリー
 Location.create(:address => '千葉県千葉市美浜区中瀬2−1') #幕張メッセ
+Location.create(:address => '京都府京都市北区金閣寺町1') #金閣寺
+Location.create(:address => '広島県広島市中区大手町1−10') #原爆ドーム
+Location.create(:address => '石川県金沢市丸の内1-1') #金沢城
